2486d66bc7ced697eae663154545e664ba45e368
SOHAM PANCHAL <panchalsoham-cmpn@atharvacoe.ac.in>
2022-11-13 21:12:22 +0530

    First Commit

---
+++ Template2/script.js
@@ -0,0 +1,516 @@
+// //container
+// var button = document.getElementsByClassName("button")[0];
+// button.addEventListener("click",function(){
+//     var but = prompt("Enter your contact number with area code:");
+//     if(but == null|| but==""){
+//         but = "ORDER : +91 83848 48492"
+//     }
+//     else{
+//         button.innerHTML = but;
+//     }
+// })
+
+//container1
+var txt1 = document.getElementById("txt1");
+txt1.addEventListener("click",function(){
+    var txt = prompt("Enter your information:(Enter character upto 410)");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst. Morbi maximus lobortis ipsum, ut blandit augue ullamcorper vitae. Nulla dignissim leo felis, eget cursus elit aliquet ut. Curabitur vel convallis massa. Morbi tellus tortor, luctus et lacinia non, tincidunt in lacus. Vivamus sed ligula imperdiet, feugiat magna vitae, blandit ex. Vestibulum id dapibus dolor, ac cursus nulla.";
+      } else {
+        txt1.innerHTML = txt;
+      }
+    
+});
+var txt2 = document.getElementById("txt2");
+txt2.addEventListener("click",function(){
+    var txt = prompt("Enter your information:(Enter character upto 410)");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst. Morbi maximus lobortis ipsum, ut blandit augue ullamcorper vitae. Nulla dignissim leo felis, eget cursus elit aliquet ut. Curabitur vel convallis massa. Morbi tellus tortor, luctus et lacinia non, tincidunt in lacus. Vivamus sed ligula imperdiet, feugiat magna vitae, blandit ex. Vestibulum id dapibus dolor, ac cursus nulla.";
+      } else {
+        txt2.innerHTML = txt;
+      }
+    
+});
+//container2
+var pizname0 = document.getElementById("pizname0");
+pizname0.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Computer Enginnering";
+      } else {
+        pizname0.innerHTML = txt;
+      }
+    
+});
+var pizname1 = document.getElementById("pizname1");
+pizname1.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Web Developer";
+      } else {
+        pizname1.innerHTML = txt;
+      }
+    
+});
+var pizname2 = document.getElementById("pizname2");
+pizname2.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Data Scientist";
+      } else {
+        pizname2.innerHTML = txt;
+      }
+    
+});
+var pizname3 = document.getElementById("pizname3");
+pizname3.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Data Entry";
+      } else {
+        pizname3.innerHTML = txt;
+      }
+    
+});
+var pizname4 = document.getElementById("pizname4");
+pizname4.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Back End-developer";
+      } else {
+        pizname4.innerHTML = txt;
+      }
+    
+});
+var pizname5 = document.getElementById("pizname5");
+pizname5.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Hotel Manager";
+      } else {
+        pizname5.innerHTML = txt;
+      }
+    
+});
+var pizname6 = document.getElementById("pizname6");
+pizname6.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Chef";
+      } else {
+        pizname6.innerHTML = txt;
+      }
+    
+});
+var pizname7 = document.getElementById("pizname7");
+pizname7.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Designer";
+      } else {
+        pizname7.innerHTML = txt;
+      }
+    
+});
+var price0 = document.getElementById("price0");
+price0.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($10,000-$50,000)";
+    } else {
+      price0.innerHTML = txt;
+    }
+  
+});
+var price1 = document.getElementById("price1");
+price1.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($5,000-$40,000)";
+    } else {
+      price1.innerHTML = txt;
+    }
+  
+});
+var price2 = document.getElementById("price2");
+price2.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto(15,000$-35,000$)";
+    } else {
+      price2.innerHTML = txt;
+    }
+  
+});
+var price3 = document.getElementById("price3");
+price3.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($5,000-$10,000)";
+    } else {
+      price3.innerHTML = txt;
+    }
+  
+});
+var price4 = document.getElementById("price4");
+price4.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($10,000-$45,000)";
+    } else {
+      price4.innerHTML = txt;
+    }
+  
+});
+var price4 = document.getElementById("price5");
+price5.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($10,000-$25,000)";
+    } else {
+      price5.innerHTML = txt;
+    }
+  
+});
+var price6 = document.getElementById("price6");
+price6.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($10,000-$30,000)";
+    } else {
+      price6.innerHTML = txt;
+    }
+  
+});
+var price7 = document.getElementById("price7");
+price7.addEventListener("click",function(){
+  var txt = prompt("Enter Salary:");
+  if (txt== null || txt == "") {
+      txt = "Upto($1,000-$30,000)";
+    } else {
+      price7.innerHTML = txt;
+    }
+  
+});
+//container3
+var pizname8 = document.getElementById("pizname8");
+pizname8.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Software Architect";
+      } else {
+        pizname8.innerHTML = txt;
+      }
+    
+});
+var pizname9 = document.getElementById("pizname9");
+pizname9.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Solutions Architect.";
+      } else {
+        pizname9.innerHTML = txt;
+      }
+    
+});
+var pizname10 = document.getElementById("pizname10");
+pizname10.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "General Surgeon";
+      } else {
+        pizname10.innerHTML = txt;
+      }
+    
+});
+var pizname11 = document.getElementById("pizname11");
+pizname11.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Lawyer";
+      } else {
+        pizname11.innerHTML = txt;
+      }
+    
+});
+var pizname12 = document.getElementById("pizname12");
+pizname12.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "IT Manager";
+      } else {
+        pizname12.innerHTML = txt;
+      }
+    
+});
+var pizname13 = document.getElementById("pizname13");
+pizname13.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Solutions IT";
+      } else {
+        pizname13.innerHTML = txt;
+      }
+    
+});
+var pizname14 = document.getElementById("pizname14");
+pizname14.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Commerical Pilot";
+      } else {
+        pizname14.innerHTML = txt;
+      }
+    
+});
+var pizname15 = document.getElementById("pizname15");
+pizname15.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Investment Analyst";
+      } else {
+        pizname15.innerHTML = txt;
+      }
+    
+});
+var pizname16 = document.getElementById("pizname16");
+pizname16.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Registered Nurse";
+      } else {
+        pizname16.innerHTML = txt;
+      }
+    
+});
+var pizname17 = document.getElementById("pizname17");
+pizname17.addEventListener("click",function(){
+    var txt = prompt("Enter Carrier Field:");
+    if (txt== null || txt == "") {
+        txt = "Acutary";
+      } else {
+        pizname17.innerHTML = txt;
+      }
+    
+});
+var price8 = document.getElementById("price8");
+price8.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$1,00,000";
+      } else {
+        price8.innerHTML = txt;
+      }
+    
+});
+var price9 = document.getElementById("price9");
+price9.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$1,20,000";
+      } else {
+        price9.innerHTML = txt;
+      }
+    
+});
+var price10 = document.getElementById("price10");
+price10.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$90,000";
+      } else {
+        price10.innerHTML = txt;
+      }
+    
+});
+var price11 = document.getElementById("price11");
+price11.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$5,00,000";
+      } else {
+        price11.innerHTML = txt;
+      }
+    
+});
+var price12 = document.getElementById("price12");
+price12.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$20,000";
+      } else {
+        price12.innerHTML = txt;
+      }
+    
+});
+var price13 = document.getElementById("price13");
+price13.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$1,20,000";
+      } else {
+        price13.innerHTML = txt;
+      }
+    
+});
+var price14 = document.getElementById("price14");
+price14.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$80,000";
+      } else {
+        price14.innerHTML = txt;
+      }
+    
+});
+var price15 = document.getElementById("price15");
+price15.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$4,50,000";
+      } else {
+        price15.innerHTML = txt;
+      }
+    
+});
+var price16 = document.getElementById("price16");
+price16.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$2,00,000";
+      } else {
+        price16.innerHTML = txt;
+      }
+    
+});
+var price17 = document.getElementById("price17");
+price17.addEventListener("click",function(){
+    var txt = prompt("Enter Salary:");
+    if (txt== null || txt == "") {
+        txt = "$1,80,000";
+      } else {
+        price17.innerHTML = txt;
+      }
+    
+});
+var des0 = document.getElementById("des0");
+des0.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des0.innerHTML = txt;
+      }
+    
+});
+var des1 = document.getElementById("des1");
+des1.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des1.innerHTML = txt;
+      }
+    
+});
+var des2 = document.getElementById("des2");
+des2.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des2.innerHTML = txt;
+      }
+    
+});
+var des3 = document.getElementById("des3");
+des3.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des3.innerHTML = txt;
+      }
+    
+});
+var des4 = document.getElementById("des4");
+des4.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des4.innerHTML = txt;
+      }
+    
+});
+var des5 = document.getElementById("des5");
+des5.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des5.innerHTML = txt;
+      }
+    
+});
+var des6 = document.getElementById("des6");
+des6.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des6.innerHTML = txt;
+      }
+    
+});
+var des6 = document.getElementById("des6");
+des6.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des6.innerHTML = txt;
+      }
+    
+});
+var des7 = document.getElementById("des7");
+des7.addEventListener("click",function(){
+    var txt = prompt("Enter your information about carrier:");
+    if (txt== null || txt == "") {
+        txt = "Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.";
+      } else {
+        des7.innerHTML = txt;
+      }
+    
+});
+//container4
+var addr = document.getElementById("addr");
+addr.addEventListener("click",function(){
+    var txt = prompt("Enter your information:(Enter character upto 40)");
+    if (txt== null || txt == "") {
+        txt = "481 Creekside Lane Avila Beach, CA 93424";
+      } else {
+        addr.innerHTML = txt;
+      }
+    
+});
+var cntc = document.getElementById("cntc");
+cntc.addEventListener("click",function(){
+    var txt = prompt("Enter your contact:");
+    if (txt== null || txt == "") {
+        txt = "+91 34678 2348";
+      } else {
+        cntc.innerHTML = txt;
+      }
+    
+});
+var eml = document.getElementById("eml");
+eml.addEventListener("click",function(){
+    var txt = prompt("Enter your email:");
+    if (txt== null || txt == "") {
+        txt = "youremail@gmail.com";
+      } else {
+        eml.innerHTML = txt;
+      }
+    
+});
